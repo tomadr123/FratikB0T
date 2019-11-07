6cb819dd74ae8c69dcafb6b594dbebe6072fd329
Mateusz Ładyko <fratik@fratikbot.pl>
2019-11-07 11:22:05 +0100

    jakim cudem u mnie nic nie wywaliło

--- commands/src/main/java/pl/fratik/commands/zabawa/ChainCommand.java
+++ commands/src/main/java/pl/fratik/commands/zabawa/ChainCommand.java
@@ -26,7 +26,6 @@ import pl.fratik.core.command.Command;
 import pl.fratik.core.command.CommandCategory;
 import pl.fratik.core.command.CommandContext;
 import pl.fratik.core.command.PermLevel;
-import pl.fratik.core.command.PremiumCommand;
 import pl.fratik.core.entity.Uzycie;
 import pl.fratik.core.util.NetworkUtil;
 
